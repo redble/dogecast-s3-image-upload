b70399bb6493783d07da58d71ac90ec4a91a9bde
redble <131158960+redble@users.noreply.github.com>
2023-06-29 22:03:38 +0800

    Update imgtp.ts

--- pages/api/imgtp.ts
+++ pages/api/imgtp.ts
@@ -1,48 +1,39 @@
 import axios from 'axios';
-import multer from 'multer';
-
-const upload = multer().single('image');
+import FormData from 'form-data';
 
 export default async function handler(req, res) {
   try {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', '*');
-
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    console.info(req);
     if (req.method !== 'POST') {
       return res.status(405).json({ error: 'Method Not Allowed' });
     }
 
-    upload(req, res, async (error) => {
-      if (error) {
-        console.error(error);
-        return res.status(400).json({ error: 'Image upload failed' });
-      }
+    const { image } = req.body;
+    if (!image) {
+      return res.status(400).json({ error: 'Image file is required' });
+    }
 
-      const { file } = req;
-      if (!file) {
-        return res.status(400).json({ error: 'Image file is required' });
-      }
+    const url = 'https://www.imgtp.com/upload/upload.html';
+    const form = new FormData();
 
-      const url = 'https://www.imgtp.com/upload/upload.html';
-      const form = new FormData();
-
-      form.append('image', file.buffer, {
-        filename: file.originalname || `${Date.now()}.png`,
-        contentType: file.mimetype,
-      });
-      form.append('fileId', `217_${file.originalname}`);
-      form.append('initialPreview', '[]');
-      form.append('initialPreviewConfig', '[]');
-      form.append('initialPreviewThumbTags', '[]');
-
-      const response = await axios.post(url, form, {
-        headers: {
-          ...form.getHeaders(),
-        },
-      });
-
-      return res.status(200).json(response.data);
+    form.append('image', Buffer.from(image, 'base64'), {
+      filename: `${Date.now()}.png`,
+      contentType: 'image/png',
     });
+    form.append('fileId', `217_image.png`);
+    form.append('initialPreview', '[]');
+    form.append('initialPreviewConfig', '[]');
+    form.append('initialPreviewThumbTags', '[]');
+
+    const response = await axios.post(url, form, {
+      headers: {
+        ...form.getHeaders(),
+      },
+    });
+
+    return res.status(200).json(response.data);
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: 'Internal Server Error' });
